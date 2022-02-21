4d9be4e16a7c22de1c6e79cbc91593f317dd6627
Anand <Anand@mail.com>
2022-02-21 13:51:18 +0530

    fact.c big.c  update added rev.c

--- fact.c
+++ fact.c
@@ -1,23 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
+void factorial() {
+    int n, i;
+    unsigned long long fact = 1;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
 
-int main(){
+    // shows error if the user enters a negative integer
+    
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %llu", n, fact);
+    }
 
-    int x=1,fact=1,n;
-
-    printf("Enter a number to find factorial: ");
-
-    scanf("%d",&n);
-
-    while(x<=n){
-
-        fact=fact*x;
-
-        x++;
-
-    } 
-
-    printf("Factorial of %d is: %d",n,fact);
-
-    return 0;
-
+    //return 0;
 }
--- rev.c
+++ rev.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
-#include <string.h>
-int main()
+reverse()
 {
-   char s[100];
+  char str[1000], rev[1000];
+  int i, j, count = 0;
+printf("\nEnter string to reverse");
+  scanf("%s", str);
+  printf("\nString Before Reverse: %s", str);
+  //finding the length of the string
+  while (str[count] != '\0')
+  {
+    count++;
+  }
+  j = count - 1;
 
-   printf("Enter a string to reverse\n");
-   gets(s);
+  //reversing the string by swapping
+  for (i = 0; i < count; i++)
+  {
+    rev[i] = str[j];
+    j--;
+  }
 
-   strrev(s);
-
-   printf("Reverse of the string: %s\n", s);
-
-   return 0;
+  printf("\nString After Reverse: %s", rev);
 }
+
